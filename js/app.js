001283e8857b59377151ea4f4ebdf65b4c517269
mcarmody <Mike@Michaels-MBP.att.net>
2014-12-18 16:30:03 -0600

    add jquery dynamic border to navbar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    console.log(scroll);
+
+    if (scroll>10) {
+    	$("#navbar").css("border-bottom", "1px solid #999")
+	} else {
+    		$("#navbar").css("border-bottom", "0px")
+	};
+});
